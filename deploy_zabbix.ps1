2d5e4a848aa28db8581aaa7c626d6d00438d110f
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-08 12:23:57 +0100

    Fixing deploy_zabbix scripts

--- deploy_zabbix.ps1
+++ deploy_zabbix.ps1
@@ -2,6 +2,6 @@
 $SOURCE_DIR=split-path -parent $MyInvocation.MyCommand.Definition
 $ZABBIX_DIR="C:\Program Files (x86)\Zabbix Agent"
 
-New-Item -Path $ZABBIX_DIR\scripts\agentd -Name msbbix -ItemType directory
-Copy-Item -Path $SOURCE_DIR\msbbix\scripts -Recurse -Destination $ZABBIX_DIR\scripts\agentd\msbbix\ -Container
-Copy-Item -Path $SOURCE_DIR\msbbix\zabbix_agentd.conf -Recurse -Destination $ZABBIX_DIR\zabbix_agentd.d\msbbix.conf -Container
+New-Item -Path $ZABBIX_DIR\scripts\agentd -Name msbbix -ItemType directory -ErrorAction Ignore
+Copy-Item -Path $SOURCE_DIR\msbbix\scripts -Recurse -Destination $ZABBIX_DIR\scripts\agentd\msbbix\ -Container -ErrorAction Ignore
+Copy-Item -Path $SOURCE_DIR\msbbix\zabbix_agentd.conf -Recurse -Destination $ZABBIX_DIR\zabbix_agentd.d\msbbix.conf -Container -ErrorAction Ignore
