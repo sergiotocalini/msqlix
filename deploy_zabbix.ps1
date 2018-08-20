29cc671a15a87680b0aa03308055f99280c89e1d
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-08-20 14:58:57 +0200

    Updating deploy_zabbix script

--- deploy_zabbix.ps1
+++ deploy_zabbix.ps1
@@ -5,4 +5,5 @@ $ZABBIX_DIR="C:\Program Files (x86)\Zabbix Agent"
 New-Item -Path $ZABBIX_DIR -Name zabbix_agentd.d -ItemType directory -ErrorAction Ignore
 New-Item -Path $ZABBIX_DIR\scripts\agentd -Name msqlix -ItemType directory -ErrorAction Ignore
 Copy-Item -Path $SOURCE_DIR\msqlix\scripts -Recurse -Destination $ZABBIX_DIR\scripts\agentd\msqlix\ -Container -ErrorAction Ignore
+Copy-Item -Path $SOURCE_DIR\msqlix\sql     -Recurse -Destination $ZABBIX_DIR\scripts\agentd\msqlix\ -Container -ErrorAction Ignore
 Copy-Item -Path $SOURCE_DIR\msqlix\zabbix_agentd.conf -Recurse -Destination $ZABBIX_DIR\zabbix_agentd.d\msqlix.conf -Container -ErrorAction Ignore
