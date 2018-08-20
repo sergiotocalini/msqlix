cec2c3da233ed1fc4357f2bf162d1ca1ac5688f9
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-08-20 14:57:58 +0200

    Updating serverproperty function

---
+++ msqlix/scripts/serverproperty.ps1
@@ -0,0 +1,13 @@
+#
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=0)][string]$property
+);
+
+if (!$property) {
+   $property = 'ProductVersion'
+}
+
+$output = Invoke-Sqlcmd -InputFile "C:\Program Files (x86)\Zabbix Agent\scripts\agentd\msqlix\sql\serverproperty.sql" -Variable @("PROP='$property'")
+$output.$property
